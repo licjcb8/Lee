a2d5e9030e78eab21ea7f679e6ce365d80f1b251
licjcb8 <38548801+licjcb8@users.noreply.github.com>
2018-04-24 12:51:15 +0900

    Test

---
+++ Test/Assets/Basic/Basic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Basic : MonoBehaviour {
+    StructA m_sStruct;
+    ClassA m_cClass;
+	// Use this for initialization
+	void Start () {
+        this.transform.position = new Vector3(0,0,0);
+        m_sStruct = new StructA(100);
+        //m_sStruct.Data = 10;
+        Debug.Log("StructData" + m_sStruct.Data);
+        m_cClass = new ClassA(100);
+        //m_cClass.Data = 20;
+        Debug.Log("ClassData" + m_cClass.Data);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Test/Assets/Basic/ClassA.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+struct StructA
+{
+    int m_nData;
+    public StructA(int data)
+    {
+        m_nData = data;
+    }
+    //void SetData(int data)
+    //{
+    //    m_nData = data;
+    //}
+    public int Data
+    {
+        get { return m_nData; }
+        set { m_nData = value; }
+    }
+}
+
+public class ClassA
+{
+    int m_nData;
+    public ClassA(int data)
+    {
+        m_nData = data;
+    }
+
+    public int Data
+    {
+        get { return m_nData; }
+        set { m_nData = value; }
+    }
+}
+
