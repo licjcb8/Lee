29b671377f51629352dfa39b723c782d4d2c0f14
licjcb8 <38548801+licjcb8@users.noreply.github.com>
2018-04-25 11:45:03 +0900

    c# understand

--- Test/Assets/Basic/Basic.cs
+++ Test/Assets/Basic/Basic.cs
@@ -7,19 +7,35 @@ using UnityEngine;
 public class Basic : MonoBehaviour {
     StructA m_sStruct;
     ClassA m_cClass;
+    List<ClassA> m_listClassA;
+    LinkedList<ClassA> m_linkedlistClassA;
+    Dictionary<string,ClassA> m_dicClassA;
 	// Use this for initialization
 	void Start () {
         this.transform.position = new Vector3(0,0,0);
         m_sStruct = new StructA(100);
         //m_sStruct.Data = 10;
+        SetDataStruct(m_sStruct, 20);
         Debug.Log("StructData" + m_sStruct.Data);
         m_cClass = new ClassA(100);
         //m_cClass.Data = 20;
+        SetDataClass(m_cClass, 10);
         Debug.Log("ClassData" + m_cClass.Data);
+        string addPlus = "A" +"d" + "d";
+        string addFormat = string.Format("{0}{1}{2}", "A", "d", "d");
+        Debug.Log(string.Format("{0}/{1}", addPlus, addFormat));
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    void SetDataStruct(StructA s, int data)
+    {
+        s.Data = data;
+    }
+
+
+    void SetDataClass(ClassA c, int data)
+    {
+        c.Data = data;
+    }
+
 }
