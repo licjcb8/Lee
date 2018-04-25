f9905fd46d60be01a16e4410989e02748604f5e6
licjcb8 <38548801+licjcb8@users.noreply.github.com>
2018-04-25 12:51:06 +0900

    Button

---
+++ Test/Assets/Basic/ButtonTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonTest : MonoBehaviour {
+
+    public Text m_cText;
+    Button m_cButton;
+	// Use this for initialization
+	void Start () {
+        m_cButton = this.GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickEvent()
+    {
+        m_cText.text = "test";
+        m_cButton.colors = ColorBlock.defaultColorBlock;
+    }
+}
