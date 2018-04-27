f3c5aeae6b30dd8c6ada76c0044a8878eff1daca
licjcb8 <38548801+licjcb8@users.noreply.github.com>
2018-04-27 12:32:51 +0900

    roomescape

---
+++ Test/Assets/prefaps/DynmicAxis.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynmicAxis : MonoBehaviour
+{
+    public float m_fSpeed = 10.0F;
+    public float m_fRotSpeed = 100.0F;
+
+    void Update()
+    {
+        float fVertical = Input.GetAxis("Vertical");
+        float fHorizontal = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(fVertical) > 0 || Mathf.Abs(fHorizontal) > 0)
+        {
+            float fTranslation = fVertical * m_fSpeed;
+            float fRotation = fHorizontal * m_fRotSpeed;
+            fTranslation *= Time.deltaTime;
+            fRotation *= Time.deltaTime;
+            transform.Translate(0, 0, fTranslation);
+            transform.Rotate(0, fRotation, 0);
+        }
+
+        GUI.Box(new Rect(0, 0, 300, 100),
+                "Vertical:" + fVertical +
+                "\nHorizontal:" + fHorizontal);
+        //float translation = Input.GetAxis("Vertical") * speed;
+        //float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        //translation *= Time.deltaTime;
+        //rotation *= Time.deltaTime;
+        //transform.Translate(0, 0, translation);
+        //transform.Rotate(0, rotation, 0);
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+}
---
+++ Test/Assets/prefaps/Test.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//C#에서 구조체는 기본접근자가 private이다.
+struct STest
+{
+    //각각 멤버마다 접근지정자를 직접 지정해야한다.
+    //생략하면 private
+    public int nData;
+}
+
+public class Test : MonoBehaviour
+{
+    public int nIdx; //인스팩터 창에 노출을 시키려면, public멤버로 만든다.
+    int nTest;
+    Rigidbody m_cRigidBody;//컴포넌트는 객체로 취급된다.
+    Test m_cTest; //클래스는 구조체가 아닌 포인터로 취급된다.
+    STest m_sTest; //구조체는 객체가 생성된다.
+    Vector3 m_vPos;
+
+    private void Awake()
+    {
+        Debug.Log("Test.Awake:" + gameObject.activeSelf);
+        this.gameObject.SetActive(true);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Test.Start:" + gameObject.activeSelf);
+        //해당 오브젝트에 GetCompoent()함수로 컴포넌트를 가져올수있다.
+        //m_cRigidBody = this.GetComponent<Rigidbody>();
+        //m_cRigidBody.useGravity = false;
+        //m_cTest = this; //자기자신의 객체의 값을 참조하게된다.
+        m_cTest.nIdx = 2;
+        m_sTest.nData = 10;
+
+        //컴포넌트는 자기를 가지고있는 게임오브젝트를 알수있다.
+        Debug.Log(m_cTest.gameObject.name);
+
+        //m_vPos = new Vector3(0, 0, 0); //구조체에 new를 할때는 동적할당하는것이아니라 생성자를 호출하는것!
+        m_vPos.x = 10;
+
+        Debug.Log("" + m_vPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nIdx == 0) //0번이 부모임으로 부모일때만 회전을 적용시킨다.
+            this.transform.Rotate(0, 1, 0);
+    }
+
+    private void OnGUI()
+    {
+        //오브젝트의 행렬을 가져온다.
+        Matrix4x4 mat = this.transform.localToWorldMatrix;
+        //자식과 부모의 UI가 겹치는 현상을 막기위해 높이를 변경함.
+        GUI.Box(new Rect(0, 80 * nIdx, 250, 80), "" + mat);
+    }
+}
